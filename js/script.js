7e0b284fcf26e13432726aec7bf121f6899edd53
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2023-08-29 21:48:52 -0700

    added button text and disappear class

--- js/script.js
+++ js/script.js
@@ -5,5 +5,15 @@ var cat = document.querySelector(".cat");
 //console.log(cat);
 
 button.addEventListener("click", function() {
-   cat.classList.add("show");
+   if (cat.classList.contains("show")) {
+      cat.classList.remove("show");
+      button.innerText = "Wait, come back!";
+      button.classList.add("disappear");
+      //console.log("no");
+   } else {
+      cat.classList.add("show");
+      button.innerText = "Shoo cat!!";
+      button.classList.remove("disappear");
+      //console.log("yes")
+   }
 });
