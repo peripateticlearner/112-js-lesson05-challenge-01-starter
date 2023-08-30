a9b6388cf96ed1a34973912e7707096a925332c8
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2023-08-29 17:41:18 -0700

    added a click event

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 var button = document.querySelector(".show-cat");
-console.log(button);
-
 var cat = document.querySelector(".cat");
-console.log(cat);
+
+//console.log(button);
+//console.log(cat);
+
+button.addEventListener("click", function() {
+   cat.classList.add("show");
+});
